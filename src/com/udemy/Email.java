579e33020b6da21f01c8b59c76cbda435e1ddb1d
evine <evinestorm@gmail.com>
2021-05-05 22:14:59 +0200

    Added setDepartment method to class Email.java

--- src/com/udemy/Email.java
+++ src/com/udemy/Email.java
@@ -13,17 +13,19 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("New email created: " + firstName + " " + lastName);
+        System.out.println("New email created: " + this.firstName + " " + this.lastName);
+
+        this.department = setDepartment();
+        System.out.println("Chosen department: " + this.department);
     }
 
     private String setDepartment() {
-        System.out.print("Enter the department:\n1 for Sales \n2 for Development\n3 for Accounting\n0 for none");
+        System.out.print("Enter the department:\n1 for Sales \n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
         Scanner in = new Scanner(System.in);
         int depChoice = in.nextInt();
         if (depChoice == 1) {return "sales"; }
         else if (depChoice == 2) {return "dev"; }
         else if (depChoice == 3) {return "acct"; }
         else {return ""; }
-
     }
 }
